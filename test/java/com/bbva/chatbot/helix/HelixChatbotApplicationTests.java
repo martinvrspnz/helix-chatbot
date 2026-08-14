44b20db9c582bb14895fc487a8b5931acabd96bb
martinvrspnz <martinvrspnz@gmail.com>
2026-08-13 20:48:58 -0500

    Add files via upload

---
+++ test/java/com/bbva/chatbot/helix/HelixChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbva.chatbot.helix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelixChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
